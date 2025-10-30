2a4e4bec177f15d5025f590154838e7d8a5d0fc5
alex <alexander.yakovlev@twerd.pl>
2025-10-30 13:31:10 +0100

    form app template

--- src/main/java/pl/torun/twerdenergoplus/ui/template/Main.java
+++ src/main/java/pl/torun/twerdenergoplus/ui/template/Main.java
@@ -3,6 +3,7 @@ package pl.torun.twerdenergoplus.ui.template;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -13,8 +14,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
         Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("Simple JavaFX App");
+        primaryStage.setTitle("TWERD ENERGO - PLUS ui application template");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(getClass().getResource("/img/TwerEnergoPlusLogo.png").toExternalForm())); // Set the application icon here
         primaryStage.show();
     }
 

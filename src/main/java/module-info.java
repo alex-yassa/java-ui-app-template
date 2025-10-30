259840d9573a1d4a1726220758ca7e45e675bfee
alex <alexander.yakovlev@twerd.pl>
2025-10-30 11:26:01 +0100

    refactor package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module ui.template {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.example to javafx.fxml, javafx.graphics;
-    exports org.example;
+    opens pl.torun.twerdenergoplus.ui.template to javafx.fxml, javafx.graphics;
+    exports pl.torun.twerdenergoplus.ui.template;
 }
